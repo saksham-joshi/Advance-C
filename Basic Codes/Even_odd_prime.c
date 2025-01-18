4ec7ae8482cf30845817714ffef56c5d835a8bc1
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-18 08:53:33 +0530

    Added: iteration count for performance analysis + Modify: 0 is a even number

--- Basic Codes/Even_odd_prime.c
+++ Basic Codes/Even_odd_prime.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 #include<time.h>
+#include<math.h>
 
-#define NEITHER_ODD_EVEN "Neither odd nor even"
 #define EVEN "Even"
 #define EVEN_AND_PRIME "Even and Prime"
 #define ODD "Odd"
 #define ODD_AND_PRIME "Odd and Prime"
 
+long long int ITERATION_COUNT;
 /*
     As per wikipedia, a Prime number is a natural number 
     which can be either divided by 1 or itself only.
 
-    So, negative numbers cannot be prime, they can be odd and prime
+    So, negative integers cannot be prime, they can be either odd or even.
 */
 inline static const char* evenOddPrime(const long long __value)
 {
-    if(__value == 0) return NEITHER_ODD_EVEN;
+    ITERATION_COUNT = 1;
+
     if(__value == 2) return EVEN_AND_PRIME;
     if(__value % 2 == 0) return EVEN;
-    if(__value < 0 ) return ODD;
+    if(__value <= 1 ) return ODD;
 
     long long limit = __value / 2;
 
-    for(long long i = 3; i <= limit ; i += 2 )
+    for(long long i = 3; i <= limit ; i += 2, ++ITERATION_COUNT)
     {
         if(__value % i == 0) return ODD;
         limit = __value / i;
@@ -33,9 +35,10 @@ inline static const char* evenOddPrime(const long long __value)
 
 int main(){
 
-    for(long long int i = -2000 ; i < 50000 ; ++i)
+    for(long long int i = 101 ; i < 120 ; ++i)
     {
-        printf("\n%lld - %s", i, evenOddPrime(i));
+        printf("\n%lld - %s", i , evenOddPrime(i));
+        printf("\t- Iter= %lld", ITERATION_COUNT);
     }
 
     printf("\n Time Taken = %f sec.", (float)clock() / (float)CLOCKS_PER_SEC);
