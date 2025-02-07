a3147cbef144716499f9cec00f6da31927aa8e3e
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-07 22:03:02 +0530

    Added: leetcode problem 66's solution

---
+++ LeetCode/66 Plus One.c
@@ -0,0 +1,68 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+inline static int* copyArray(int* __array , unsigned short int __len , int __some_non_zero_value)
+{
+    int *ar = ( int *) calloc ( __len , sizeof(int) );
+
+    int* iterator = ar;
+
+    if ( __some_non_zero_value ) *iterator++ = __some_non_zero_value;
+
+    for ( const int*const end = ar + __len ; iterator < end ;  ++ __array , ++iterator ) *iterator = *__array;
+    
+    return ar;
+}
+
+
+inline static int* plusOne(int* __digits, unsigned short int __len, int* __return_size)
+{
+    int* reverse_iterator = __digits + __len - 1;
+
+    if ( *reverse_iterator != 9 )
+    {
+        ++ ( *reverse_iterator );
+
+        *__return_size = __len;
+
+        return copyArray(__digits, __len , 0);
+    }
+
+    int carry = 0;
+
+    if (*reverse_iterator == 9 )
+    {
+        *reverse_iterator = 0;
+        carry = 1;
+        --reverse_iterator;
+    }
+
+    while ( reverse_iterator >= __digits )
+    {
+        if ( *reverse_iterator != 9)
+        {
+            *reverse_iterator += carry;
+
+            *__return_size = __len;
+
+            return copyArray(__digits , __len , 0);
+        }
+
+        *reverse_iterator += carry;
+
+        if (*reverse_iterator >= 10)
+        {
+            carry = 1;
+            *reverse_iterator %= 10;
+        }
+        else carry = 0;
+
+        -- reverse_iterator;
+    }
+
+    *__return_size = __len;
+
+    if (! carry) return __digits;
+
+    return copyArray(__digits , ++(*__return_size) , carry);
+}
