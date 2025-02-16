39afdf5610b6e5884fc7f6b0293e28f081620ad3
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-16 18:34:38 +0530

    Updated: Base_LL.h for genericity

--- DSA Codes/LinkedList/Base_LL.h
+++ DSA Codes/LinkedList/Base_LL.h
@@ -8,41 +8,7 @@
 #include<errno.h>
 
 #define NodeData_t int
-
-#if defined(NodeData_t)
-        #if NodeData_t == int
-            #define FORMAT_SPECIFIER_LINKED_LIST "%d"
-        #elif NodeData_t == unsigned int
-            #define FORMAT_SPECIFIER_LINKED_LIST "%u"
-        #elif NodeData_t == short int
-            #define FORMAT_SPECIFIER_LINKED_LIST "%hd"
-        #elif NodeData_t == unsigned short int
-            #define FORMAT_SPECIFIER_LINKED_LIST "%hu"
-        #elif NodeData_t == long
-            #define FORMAT_SPECIFIER_LINKED_LIST "%ld"
-        #elif NodeData_t == long long
-            #define FORMAT_SPECIFIER_LINKED_LIST "%lld"
-        #elif NodeData_t == unsigned long long
-            #define FORMAT_SPECIFIER_LINKED_LIST "%llu"
-        #elif NodeData_t == char
-            #define FORMAT_SPECIFIER_LINKED_LIST "%c"
-        #elif NodeData_t == float
-            #define FORMAT_SPECIFIER_LINKED_LIST "%f"
-        #elif NodeData_t == long float
-            #define FORMAT_SPECIFIER_LINKED_LIST "%lf"
-        #elif NodeData_t == double
-            #define FORMAT_SPECIFIER_LINKED_LIST "%f"
-        #elif NodeData_t == long double
-            #define FORMAT_SPECIFIER_LINKED_LIST "%Lf"
-        #else
-            #define FORMAT_SPECIFIER_LINKED_LIST "%s"
-        #endif
-
-#else
-        
-    #error "You have to define 'NodeData_t to work on Doubly Linked List!"
-
-#endif
+#define IS_NODE_DATA_PRINTABLE 1    // set it 0 if the value is not printable otherwise set it one
 
 
 #if defined(SINGLY_LL)
@@ -61,32 +27,49 @@
         NodeData_t _data;
         struct Node* _next;
     };
-
 #elif defined(MULTILEVEL_LL)
 
-    struct MultilevelLinkedList;
-
-    struct Node
-    {
-        struct MultilevelLinkedList* _child;
-        NodeData_t _data;
-        struct Node* _next;
-    };
-
-    struct MultilevelLinkedList
-    {
-        size_t _len;
-        struct Node* _head;
-        struct Node* _tail;
-    };
-
-    typedef struct MultilevelLinkedList* MlList;
-
+    #undef NodeData_t
+    #define NodeData_t char
 #else
-
-    #error "At least one of type of Linked List must be defined ...!"
-
+    #error "Among 'SINGLY_LL', 'DOUBLE_LL', 'MULTILEVEL_LL' at least one of type of Linked List must be defined!"
 #endif 
+
+#if IS_NODE_DATA_PRINTABLE
+    #if defined(NodeData_t)
+            #if NodeData_t == int
+                #define FORMAT_SPECIFIER_LINKED_LIST "%d"
+            #elif NodeData_t == unsigned int
+                #define FORMAT_SPECIFIER_LINKED_LIST "%u"
+            #elif NodeData_t == short int
+                #define FORMAT_SPECIFIER_LINKED_LIST "%hd"
+            #elif NodeData_t == unsigned short int
+                #define FORMAT_SPECIFIER_LINKED_LIST "%hu"
+            #elif NodeData_t == long
+                #define FORMAT_SPECIFIER_LINKED_LIST "%ld"
+            #elif NodeData_t == long long
+                #define FORMAT_SPECIFIER_LINKED_LIST "%lld"
+            #elif NodeData_t == unsigned long long
+                #define FORMAT_SPECIFIER_LINKED_LIST "%llu"
+            #elif NodeData_t == char
+                #define FORMAT_SPECIFIER_LINKED_LIST "%c"
+            #elif NodeData_t == float
+                #define FORMAT_SPECIFIER_LINKED_LIST "%f"
+            #elif NodeData_t == long float
+                #define FORMAT_SPECIFIER_LINKED_LIST "%lf"
+            #elif NodeData_t == double
+                #define FORMAT_SPECIFIER_LINKED_LIST "%f"
+            #elif NodeData_t == long double
+                #define FORMAT_SPECIFIER_LINKED_LIST "%Lf"
+            #else
+                #define FORMAT_SPECIFIER_LINKED_LIST "%s"
+            #endif
+
+    #else   
+        #error "You have to define 'NodeData_t to work on any Linked List!"
+    #endif
+#endif
+
 
 
 #define LINKED_LIST_DEBUG_MODE 1
