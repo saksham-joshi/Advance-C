a4a60ad73acc6360023655270d36021ec6f3f6be
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-26 09:31:18 +0530

    Removed: Printable definition + Minor changes

--- DSA Codes/LinkedList/Base_LL.h
+++ DSA Codes/LinkedList/Base_LL.h
@@ -1,16 +1,13 @@
 #pragma once
-#pragma clang optimize("-O3")
+#pragma clang optimize on
 
 #ifndef _BASE_LL_H_
 #define _BASE_LL_H_
 
-#include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
 
 #define NodeData_t int
-#define IS_NODE_DATA_PRINTABLE 1    // set it 0 if the value is not printable otherwise set it one
-
 
 #if defined(SINGLY_LL)
 
@@ -20,6 +17,23 @@
         struct Node* _next;
     };
 
+    typedef struct Node* NodePtr_t;
+
+    struct LinkedList {
+        size_t _len;
+        NodePtr_t _head;
+        NodePtr_t _tail;
+    };
+
+    inline struct LinkedList makeLinkedList()
+    {
+        struct LinkedList obj = {0};
+
+        return obj;
+    }
+    
+    typedef struct LinkedList* LLPtr;
+
 #elif defined(DOUBLE_LL)
     
     struct Node
@@ -28,47 +42,33 @@
         NodeData_t _data;
         struct Node* _next;
     };
+
+    typedef struct Node* NodePtr_t;
+
+    struct DoublyLinkedList
+    {
+        NodePtr_t _head;
+        NodePtr_t _tail;
+        size_t _len;
+    };
+
+    typedef struct DoublyLinkedList* DLLptr_t;
+
+    inline static struct DoublyLinkedList makeDoublyLinkedList(void)
+    {
+        struct DoublyLinkedList obj = {0};
+
+        return obj;
+    }
+
 #elif defined(MULTILEVEL_LL)
 
     #undef NodeData_t
     #define NodeData_t char
+
 #else
     #error "Among 'SINGLY_LL', 'DOUBLE_LL', 'MULTILEVEL_LL' at least one of type of Linked List must be defined!"
 #endif 
-
-#if IS_NODE_DATA_PRINTABLE
-    #if defined(NodeData_t)
-            #if NodeData_t == int
-                #define FORMAT_SPECIFIER_LINKED_LIST "%d"
-            #elif NodeData_t == unsigned int
-                #define FORMAT_SPECIFIER_LINKED_LIST "%u"
-            #elif NodeData_t == short int
-                #define FORMAT_SPECIFIER_LINKED_LIST "%hd"
-            #elif NodeData_t == unsigned short int
-                #define FORMAT_SPECIFIER_LINKED_LIST "%hu"
-            #elif NodeData_t == long int
-                #define FORMAT_SPECIFIER_LINKED_LIST "%ld"
-            #elif NodeData_t == long long
-                #define FORMAT_SPECIFIER_LINKED_LIST "%lld"
-            #elif NodeData_t == unsigned long long
-                #define FORMAT_SPECIFIER_LINKED_LIST "%llu"
-            #elif NodeData_t == char
-                #define FORMAT_SPECIFIER_LINKED_LIST "%c"
-            #elif NodeData_t == float
-                #define FORMAT_SPECIFIER_LINKED_LIST "%f"
-            #elif NodeData_t == double
-                #define FORMAT_SPECIFIER_LINKED_LIST "%lf"
-            #elif NodeData_t == long double
-                #define FORMAT_SPECIFIER_LINKED_LIST "%Lf"
-            #else
-                #define FORMAT_SPECIFIER_LINKED_LIST "%s"
-            #endif
-
-    #else   
-        #error "You have to define 'NodeData_t to work on any Linked List!"
-    #endif
-#endif
-
 
 
 #define LINKED_LIST_DEBUG_MODE 1
@@ -84,17 +84,6 @@ inline void Utility_destroyRecursively(NodePtr_t __ptr)
     Utility_destroyRecursively(__ptr->_next);
 
     free(__ptr);
-}
-
-inline NodeData_t getInput(const char* __str)
-{
-    printf("%s", __str);
-
-    NodeData_t value;
-
-    scanf_s(FORMAT_SPECIFIER_LINKED_LIST, &value);
-
-    return value;
 }
 
 #endif
