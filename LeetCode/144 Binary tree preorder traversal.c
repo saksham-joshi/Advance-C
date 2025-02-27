9d47f73507c7bffa540a7de781a7b7f66d444ad3
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-27 11:12:07 +0530

    Added: leetcode problem 144's solution

---
+++ LeetCode/144 Binary tree preorder traversal.c
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define BaseVector_t int 
+#define VectorLen_t unsigned long
+
+typedef struct 
+{
+    BaseVector_t* _array;
+    VectorLen_t _len;
+    VectorLen_t _allocated;
+} Vector ;
+
+typedef Vector* VectorPtr_t;
+
+inline static Vector vectorInit( VectorLen_t __allocate )
+{
+    Vector obj = { 0 };
+
+    obj._array = ( BaseVector_t *) calloc ( __allocate , sizeof ( BaseVector_t ) );
+
+    obj._allocated = __allocate;
+
+    return obj;
+}
+
+inline static void vectorPushBack( VectorPtr_t __vector , BaseVector_t __value )
+{
+    if ( __vector->_len == __vector->_allocated )
+    {
+        BaseVector_t* old_ar_iterator = __vector->_array;
+
+        BaseVector_t* new_array = ( BaseVector_t * ) calloc ( __vector->_allocated * 2, sizeof ( BaseVector_t ) );
+
+        BaseVector_t* new_arr_iterator = new_array;
+
+        for ( VectorLen_t i = 0 ; i < __vector->_len ; ++i , ++old_ar_iterator , ++new_arr_iterator ) *new_arr_iterator = *old_ar_iterator;
+
+        free ( __vector->_array );
+
+        __vector->_array = new_array;
+
+        __vector->_allocated *= 2;
+    }
+
+    __vector->_array [ __vector->_len++ ] = __value;
+
+}
+static void basePreOrder ( struct TreeNode* __node , VectorPtr_t __vector )
+{
+    if ( __node )
+    {
+        vectorPushBack ( __vector , __node->val );
+        basePreOrder   ( __node->left , __vector );
+        basePreOrder   ( __node->right , __vector );
+    }
+} 
+inline static int* preorderTraversal(struct TreeNode* __root, int* __return_size)
+{
+    Vector vec = vectorInit ( 4 );
+
+    basePreOrder ( __root , &vec );
+
+    *__return_size = vec._len;
+
+    return vec._array;
+}
