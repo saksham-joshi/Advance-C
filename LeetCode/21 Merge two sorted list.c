13a7420f3add9bdb6e3552b7cad46e1c2f371fa6
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2026-01-21 23:41:38 +0530

    Optimized: leetcode 21's solution

--- LeetCode/21 Merge two sorted list.c
+++ LeetCode/21 Merge two sorted list.c
@@ -5,61 +5,66 @@
  *     struct ListNode *next;
  * };
  */
-typedef struct ListNode* NodePtr_t;
-
-typedef struct
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
 {
-    NodePtr_t _head;
-    NodePtr_t _tail;
-
-} LinkedList;
-
-
-inline static void pushBack(LinkedList* __obj , int __val)
-{
-
-    NodePtr_t temp = (NodePtr_t) malloc(sizeof(struct ListNode));
-    temp->val = __val;
-    temp->next = NULL;
-
-    if( ! __obj->_head)     __obj->_head = temp;
-
-    else                    __obj->_tail->next = temp; 
-        
-    __obj->_tail = temp;
-
-}
-
-inline static struct ListNode* mergeTwoLists(struct ListNode* __list1, struct ListNode* __list2)
-{
-    LinkedList output = { 0 };
-
-    while( __list1 || __list2 )
+    if(list1 && list2)
     {
-        if ( __list1 && __list2 )
+        struct ListNode* output_head;
+        
+        // assigning output_head
+        if(list1->val < list2->val)
         {
-            if (__list1->val < __list2->val)
-            {
-                pushBack(&output , __list1->val);
-                __list1 = __list1->next;
-            }
-            else
-            {
-                pushBack(&output , __list2->val);
-                __list2 = __list2->next;
-            }
-        }
-        else if( __list1 )
-        {
-            pushBack(&output , __list1->val);
-            __list1 = __list1->next;
+            output_head = list1;
+
+            list1 = list1->next;
         }
         else
         {
-            pushBack(&output , __list2->val);
-            __list2 = __list2->next;
+            output_head = list2;
+
+            list2 = list2->next;
         }
+
+        struct ListNode* output_tail = output_head;
+
+        while(list1 && list2)
+        {
+            if(list1->val < list2->val)
+            {
+                output_tail->next = list1;
+
+                list1 = list1->next;
+            }
+            else
+            {
+                output_tail->next = list2;
+
+                list2 = list2->next;
+            }
+
+            output_tail = output_tail->next;
+        }
+
+        while(list1)
+        {
+            output_tail->next = list1;
+
+            list1 = list1->next;
+
+            output_tail = output_tail->next;
+        }
+
+        while(list2)
+        {
+            output_tail->next = list2;
+
+            list2 = list2->next;
+
+            output_tail = output_tail->next;
+        }
+
+        return output_head;
     }
 
-    return output._head;
+    return (list1) ? list1 : list2;
 }
